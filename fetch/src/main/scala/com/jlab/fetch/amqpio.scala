49a9c81e6f8e0eb366bb726c932e1e39edd873f3
tien nguyen <nvtie@yahoo.com>
2014-12-30 11:07:02 +0700

    chan --> channel

--- fetch/src/main/scala/com/jlab/fetch/amqpio.scala
+++ fetch/src/main/scala/com/jlab/fetch/amqpio.scala
@@ -36,18 +36,24 @@ class amqpBatchPrefetcher(config: Config
   }
 
   override def getBatch(sz:Int):Option[List[Data]] = {
-    def rec(c:Int, l:List[Data]):List[Data] = { try { if(0==c) { l } else { rec(c-1, getMessage()::l ) } } catch { case e:EmptyQueue => l } }
+    def rec(c:Int, l:List[Data]):List[Data] = { 
+      try { 
+        if (0 == c) { l } 
+        else { rec(c-1, getMessage()::l ) } 
+      } 
+      catch { case e:EmptyQueue => l } 
+    }
 
-    val l= rec(sz, List[Data]())
-    val ls= l.length
-    if(ls>0) { log.info("Fetched "+ls.toString+" message(s) from "+queue) }
-    if(l isEmpty) { None } else { return Some(l) }
+    val l = rec(sz, List[Data]())
+    val ls = l.length
+    if (ls > 0) { log.info("Fetched "+ls.toString+" message(s) from "+queue) }
+    if (l isEmpty) { None } else { return Some(l) }
   }
   
 }
 
 // Writing back to an AMQP exchange
-class amqpBatchWriteback(config: Config, control: Control, chan: Channel) extends genericBatchReseller[Data](control) {
+class amqpBatchWriteback(config: Config, control: Control, channel: Channel) extends genericBatchReseller[Data](control) {
 
   //val log = Logging(context.system, this)
   val exch = config("exchange")
@@ -56,7 +62,7 @@ class amqpBatchWriteback(config: Config, control: Control, chan: Channel) extend
   def resell(batch: List[Data]) = {  
     batch match {
       case x::xs => {
-        val deliveryTag= (x get "fetch_queue_delivery_tag").toLong
+        val deliveryTag = (x get "fetch_queue_delivery_tag").toLong
         val key= try {
             (x get "fetch_status_code") match {
               case "200" => "200"
@@ -69,8 +75,8 @@ class amqpBatchWriteback(config: Config, control: Control, chan: Channel) extend
 //        log.info(x(fqp))
         val fullkey = if(x exists fqp) { x(fqp)+ ":" + key } else { key }
 //        log.info(x("fetch_data"))
-        chan.basicPublish(exch, fullkey, MessageProperties.PERSISTENT_TEXT_PLAIN, x.toBytes)
-        chan.basicAck(deliveryTag, false)
+        channel.basicPublish(exch, fullkey, MessageProperties.PERSISTENT_TEXT_PLAIN, x.toBytes)
+        channel.basicAck(deliveryTag, false)
         log.info("Publishing message to " + exch + " and acking delivery tag " + deliveryTag)
         resell(xs) 
       }
