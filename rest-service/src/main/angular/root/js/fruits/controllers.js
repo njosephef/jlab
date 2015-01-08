bdf5206bb6692a99fba3a7df61c25115240bee9e
scorpiovn <nvtie@yahoo.com>
2015-01-08 23:30:32 +0700

    angularjs

--- rest-service/src/main/angular/root/js/fruits/controllers.js
+++ rest-service/src/main/angular/root/js/fruits/controllers.js
@@ -1,5 +1,7 @@
-angular.module('fruitapp').controller('fruits-controller', function($scope, Restangular) {
-    $scope.p = Restangular.all('fruits').getList().then(function(fruitlist) {
-        $scope.fruitlist = fruitlist
+angular
+    .module('fruitapp')
+        .controller('fruits-controller', function($scope, Restangular) {
+            $scope.p = Restangular.all('fruits').getList().then(function(fruitlist) {
+            $scope.fruitlist = fruitlist
     });
 });
